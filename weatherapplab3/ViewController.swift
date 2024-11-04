7638385f4cf8aa2233fcddf23997b5f3857e465c
ibrahim <abbamuhtar@gmail.com>
2024-11-04 13:18:41 -0500

    Initial Commit

---
+++ weatherapplab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherapplab3
+//
+//  Created by Ibrahim muhtar on 2024-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weatherapplab3Tests/weatherapplab3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherapplab3Tests.swift
+//  weatherapplab3Tests
+//
+//  Created by Ibrahim muhtar on 2024-11-04.
+//
+
+import Testing
+@testable import weatherapplab3
+
+struct weatherapplab3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
